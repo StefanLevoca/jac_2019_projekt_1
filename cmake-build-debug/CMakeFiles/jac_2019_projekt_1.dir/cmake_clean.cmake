f148dbf32a543adcdb44c5ef3f17c4f89b41451e
Štefan Levoča <stefanlevoca@gmail.com>
2019-11-15 19:51:46 +0100

    1. project from JAC

---
+++ cmake-build-debug/CMakeFiles/jac_2019_projekt_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jac_2019_projekt_1.dir/main.c.obj"
+  "CMakeFiles/jac_2019_projekt_1.dir/program.c.obj"
+  "jac_2019_projekt_1.exe"
+  "jac_2019_projekt_1.exe.manifest"
+  "jac_2019_projekt_1.pdb"
+  "libjac_2019_projekt_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jac_2019_projekt_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
